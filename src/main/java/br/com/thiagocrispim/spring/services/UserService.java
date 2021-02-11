e672a2299650c1090673215c2097c108b2d4bfb3
Thiago Jorge Crispim <thiagojcrispim@yahoo.com.br>
2021-02-11 20:41:29 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/br/com/thiagocrispim/spring/services/UserService.java
@@ -0,0 +1,27 @@
+package br.com.thiagocrispim.spring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.thiagocrispim.spring.entities.User;
+import br.com.thiagocrispim.spring.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
